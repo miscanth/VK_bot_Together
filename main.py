7b97580f36ce036085bafb3c60301d931b32bd6d
Ermachkova Julia <miscanth@gmail.com>
2024-05-21 14:07:18 +0300

    Refactoring and rewrite read.me

--- main.py
+++ main.py
@@ -1,44 +1,37 @@
 import os
-import requests
 
 from dotenv import load_dotenv
 import vk_api
-# from vk_api import VkUpload
 from vk_api.longpoll import VkLongPoll, VkEventType
 from vk_api.utils import get_random_id
 
 
 load_dotenv()
 
+greetings = ['hello', 'hi', 'привет', 'здравствуйте', 'hallo', 'gamarjoba',
+             'shalom', 'hola', 'namaste', 'salut', 'bonjour', 'konnichiwa']
+
 
 def main():
-    # session = requests.Session()
-
-    # Авторизация группы (для групп рекомендуется использовать VkBotLongPoll):
-    # при передаче token вызывать vk_session.auth не нужно
-
     vk_session = vk_api.VkApi(token=os.environ['TOKEN'])
     vk = vk_session.get_api()
 
-    # upload = VkUpload(vk_session)  # Для загрузки изображений
     longpoll = VkLongPoll(vk_session)
 
     for event in longpoll.listen():
         if event.type == VkEventType.MESSAGE_NEW and event.to_me and event.text:
             print(f'id{event.user_id}: "{event.text}"', end=' ')
 
-            # if event.text == 'Hello' or event.text == 'Hi': # Если написали заданную фразу
-            if 'Hello' in event.text:
+            if event.text.lower() in greetings:
                 if event.from_user: # Если написали в ЛС
-                    vk.messages.send( # Отправляем сообщение
+                    vk.messages.send(
                         user_id=event.user_id,
                         random_id=get_random_id(),
-                        message='Здравствуйте и до свидания!'
+                        message='Здравствуйте и до свидания! Я пока не очень умный.'
                     )
-                    print('Hello-hello!')
 
                 elif event.from_chat: # Если написали в Беседе
-                    vk.messages.send( # Отправляем собщение
+                    vk.messages.send(
                         chat_id=event.chat_id,
                         random_id=get_random_id(),
                         message='Ваш текст in chat'
@@ -54,3 +47,4 @@ def main():
 
 if __name__ == '__main__':
     main()
+ 
